64aeef069ac42bd06de3f8bf18bb19bcec368cb4
YunSungBin <dbstjdqls14@naver.com>
2026-06-29 15:05:15 +0900

    Initialize answer code: src/main/java/com/mmcafe/MmcafeApplication.java

---
+++ src/main/java/com/mmcafe/MmcafeApplication.java
@@ -0,0 +1,9 @@
+package com.mmcafe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MmcafeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MmcafeApplication.class, args);
+    }
+}
